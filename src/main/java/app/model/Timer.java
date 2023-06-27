73a81620ebffd65bdd9e5c14c03259e07239d1c6
Иван <streeeees13.s@gmail.com>
2023-06-27 19:28:18 +0300

    com

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,13 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
-
+@Autowired
     public Long getTime() {
         return nanoTime;
     }
